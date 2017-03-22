e945fff674dee4fe74c1ab0b844f1334b9e907cf
Python _data_processing <python_data_processing@python-data-processings-macbook-air.local>
2017-03-22 21:15:43 +0300

    Initial Commit

---
+++ weird_story_generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weird_story_generator
+//
+//  Created by Python_data_processing on 22/03/2017.
+//  Copyright © 2017 Python_data_processing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
